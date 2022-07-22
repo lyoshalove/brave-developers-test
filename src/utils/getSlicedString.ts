ffb7480c5458ddad5bc3501f39049e80ffd5a2f7
lyosha <78252352+lyoshalove@users.noreply.github.com>
2022-07-22 22:39:17 +0400

    update getSlicedString function

--- src/utils/getSlicedString.ts
+++ src/utils/getSlicedString.ts
@@ -1,6 +1,6 @@
 export function sliceString(name: string) {
-  if (name.length > 9) {
-    return `${name.slice(0, 9)}...`;
+  if (name.length > 7) {
+    return `${name.slice(0, 7)}...`;
   }
   return name;
 }
